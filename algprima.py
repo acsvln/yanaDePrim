59a4d7d2e95d01b90b455e7684c9321e6fe399a6
Unknown <acsvln@disroot.org>
2022-07-09 01:38:36 +0300

    Added proto

---
+++ algprima.py
@@ -0,0 +1,14 @@
+def link_vertex(v, u):
+    pass
+
+def random_vertex():
+    pass
+
+def sibling_vertex():
+    pass
+
+def prima():
+    v=random_vertex()
+    u=sibling_vertex()
+    link_vertex(v, u)
+    pass
